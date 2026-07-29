7217cca04a2aa296eebd1763870e12ee94cc7eea
Jovany George <jovanytechn6ologygeorge11@gmail.com>
2026-07-29 12:50:23 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const navbarButton=document.querySelector(".navbar-button")
-const navbar=document.querySelector("nav")
-navbarButton.addEventListener("click",()=>{
-    navbar.classList.toggle("full-nav-mobile");
-})
- document.querySelector(".copyright").textContent +=
-        ` ${new Date().getFullYear()}`;
