3543239456015d09b1067f4b77e12ff78be6417d
jovanyweb <jovanytechn6ologygeorge11@gmail.com>
2026-08-18 22:38:21 +0300

    Mobile responsive 2

---
+++ js/script.js
@@ -0,0 +1,2 @@
+let navBtn=document.querySelector("nav button"),navLinks=document.querySelector("nav");navBtn.addEventListener("click",()=>{navLinks.classList.toggle("active")}),window.addEventListener("resize",()=>{navLinks.classList.remove("active")});
+//# sourceMappingURL=script.js.map
