23f50ad5b803a8b587c9c51a59dbbc014dd5a219
jovanyweb <jovanytechn6ologygeorge11@gmail.com>
2026-08-18 22:41:16 +0300

    Mobile responsive 2

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
-let navBtn=document.querySelector("nav button"),navLinks=document.querySelector("nav");navBtn.addEventListener("click",()=>{navLinks.classList.toggle("active")}),window.addEventListener("resize",()=>{navLinks.classList.remove("active")});
+let navBtn=document.querySelector("nav button"),navLinks=document.querySelector("nav");navBtn.addEventListener("click",()=>{navLinks.classList.toggle("active")}),window.addEventListener("resize",()=>{navLinks.classList.remove("active"),navBtn.style.display="none"});
 //# sourceMappingURL=script.js.map
